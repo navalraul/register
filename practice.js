a33dff22fa19fa7753dbe3135277a068c1b50e74
navalraul <129669942+navalraul@users.noreply.github.com>
2023-04-22 12:19:42 +0530

    update

--- practice.js
+++ practice.js
@@ -1,3 +1,5 @@
+
+
 function login(event) {
     event.preventDefault();
     // alert("function called")
@@ -10,52 +12,6 @@ function login(event) {
     var confirmpassword = document.getElementById("userconfirmpassword").value
     // console.log(confirmpassword);
 
-    if (name && email && password && confirmpassword) {
-        if (password.length >= 8 && confirmpassword >= 8) {
-
-            if (password == confirmpassword) {
-
-                var Ls = JSON.parse(localStorage.getItem("Users")) || []
-
-                var currentUser;
-                var flag = false;
-                for (var i = 0; i < Ls.length; i++) {
-                    if (Ls[i].userEmail == email) {
-                        flag = true;
-                        currentUser =Ls[i];
-                    }
-                }
-                if (!flag) {
-                    var userdata = {
-                        userName: name,
-                        userEmail: email,
-                        userPassword: password,
-                        userConfirmPassword: confirmpassword
-                    }
-
-                    localStorage.setItem("currentUser", JSON.stringify(currentUser))
-                    window.location.href = './home.html'
-                    // Ls.push(userdata);
-                    localStorage.setItem("Users", JSON.stringify(Ls))
-                    alert("Registration Successful")
-                    document.getElementById("username").value = ""
-                    document.getElementById("useremail").value = ""
-                    document.getElementById("userpassword").value = ""
-                    document.getElementById("userconfirmpassword").value = ""
-                }
-                else {
-                    alert("Email aleready exist")
-                }
-            }
-            else {
-                console.log("password not match");
-            }
-
-        } else {
-            console.log("password should  include 8 or more characters");
-        }
-    } else {
-        console.log("please fill all fields");
-    }
+    
 
 }
